d5676d3528bb990d3cbc2c7e444e2a268ab3b08e
XPLOIT-123 <85129943+XPLOIT-123@users.noreply.github.com>
2024-02-09 21:53:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let currentMessageIndex = 0;
 
 const messages = [
-  "Hello there!",
-  "You clicked the screen!",
-  "This is a message.",
-  "Click again for more messages!"
+  "You are my sunshine!",
+  "You make my heart skip a beat!",
+  "I love you more than words can express!",
+  "You are the best thing that ever happened to me!",
+  "Every moment with you is precious!"
 ];
 
 function displayMessage() {
